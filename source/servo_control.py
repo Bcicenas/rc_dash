c9e6950e8d60dfe6de070a79ae6674ff81f01068
Benediktas Cicėnas <bcicenas@gmail.com>
2022-01-11 22:00:00 +0200

    Code cleanup for servo_control.py

--- source/servo_control.py
+++ source/servo_control.py
@@ -3,14 +3,20 @@ import serial
 import time
 import ps4_controller
 import struct
+
+# Get active ps4 controllers
 ps4_controller.get_joystics()
+
+# Open serial connection beetwen PC and Arduino
 arduino = serial.Serial(port='COM4', baudrate=9600, timeout=.1)
+
+# function to send and read data from/to Arduino
 def write_read(x):
 	arduino.write(struct.pack("BBB", x[0], x[1], x[2]))
-	
 	data = arduino.readline()
 	return data
 
+# Remaps ps4 controller values to specific one
 def remap_value(x):
 	if x == -1:
 		return 1
@@ -19,19 +25,11 @@ def remap_value(x):
 
 	return 2
 
+# Main loop for sending and receiving data
 while True:
 	ps4_controller.check_for_button_press()
-
-	# num = input("Enter a number: ") # Taking input from user
-	# value = write_read(str(int(ps4_controller.analog_buttons[0])))
-	# print(ps4_controller.analog_buttons[0])	
-	#-1*int(ps4_controller.analog_buttons[5])
 	control_array = [remap_value(int(ps4_controller.analog_buttons[0])), remap_value(int(ps4_controller.analog_buttons[5])), remap_value(int(ps4_controller.analog_buttons[4]))]
-	value = write_read(control_array)
-	# print(int(ps4_controller.analog_buttons[0]), int(ps4_controller.analog_buttons[5]))
-	# print(control_array)
-	print(remap_value(int(ps4_controller.analog_buttons[0])))
-	
+	write_read(control_array)
 	time.sleep(0.1)
 
 
